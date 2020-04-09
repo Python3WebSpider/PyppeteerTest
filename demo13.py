5605948cec51fc8a02f396cca98823203333440c
Germey <cqc@cuiqingcai.com>
2020-04-10 00:12:35 +0800

    update

--- demo13.py
+++ demo13.py
@@ -1,17 +1,15 @@
 import asyncio
-from typing import Dict
-
 from pyppeteer import launch
-from pyquery import PyQuery as pq
 
 
 async def main():
     browser = await launch()
     page = await browser.newPage()
+    await page.authenticate({'username': 'admin', 'password': 'admin'})
     await page.goto('https://static3.scrape.cuiqingcai.com/')
-    await page.authenticate(Dict['admin', 'admin'])
     print(await page.content())
     await browser.close()
 
 
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
